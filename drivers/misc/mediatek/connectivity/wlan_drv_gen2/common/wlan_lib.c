42f33be66d796066f6af817f6bc6f86b882b2f6d
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-12-18 11:08:03 +0000

    drivers: mediatek: wlan_drv_gen2: Force 5GHz Wi-Fi support
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- drivers/misc/mediatek/connectivity/wlan_drv_gen2/common/wlan_lib.c
+++ drivers/misc/mediatek/connectivity/wlan_drv_gen2/common/wlan_lib.c
@@ -3842,14 +3842,7 @@ WLAN_STATUS wlanLoadManufactureData(IN P_ADAPTER_T prAdapter, IN P_REG_INFO_T pr
 
 	/* 3. Check if needs to support 5GHz */
 	/* if(prRegInfo->ucEnable5GBand) { // Frank workaround */
-	if (1) {
-		/* check if it is disabled by hardware */
-		if (prAdapter->fgIsHw5GBandDisabled || prRegInfo->ucSupport5GBand == 0)
-			prAdapter->fgEnable5GBand = FALSE;
-		else
-			prAdapter->fgEnable5GBand = TRUE;
-	} else
-		prAdapter->fgEnable5GBand = FALSE;
+	prAdapter->fgEnable5GBand = TRUE;
 
 	/*
 	 * DBGLOG(INIT, INFO, "NVRAM 5G Enable(%d) SW_En(%d) HW_Dis(%d)\n",
