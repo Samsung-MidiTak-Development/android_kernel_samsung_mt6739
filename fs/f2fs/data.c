bd9ceae235eb273628678f9a02e44d0f4821b8bc
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-12-28 16:46:04 +0000

    Revert "[ALPS05007074] crypto: Revert MediaTek HIE-related patch"
    This reverts commit d061a69191459c0a2d4b4cb6936c5c4fa5ab7b7b.

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -17,6 +17,7 @@
 #include <linux/prefetch.h>
 #include <linux/uio.h>
 #include <linux/cleancache.h>
+#include <linux/hie.h>
 #include <linux/sched/signal.h>
 
 #include "f2fs.h"
@@ -138,6 +139,9 @@ static void bio_post_read_processing(struct bio_post_read_ctx *ctx)
 
 static bool f2fs_bio_post_read_required(struct bio *bio)
 {
+	if (bio_encrypted(bio))
+		return false;
+
 	return bio->bi_private && !bio->bi_status;
 }
 
@@ -381,6 +385,7 @@ static void __submit_merged_bio(struct f2fs_bio_info *io)
 	else
 		trace_f2fs_prepare_write_bio(io->sbi->sb, fio->type, io->bio);
 
+	f2fs_set_bio_ctx_fio(fio, io->bio);
 	__submit_bio(io->sbi, io->bio, fio->type);
 	io->bio = NULL;
 }
@@ -514,9 +519,55 @@ int f2fs_submit_page_bio(struct f2fs_io_info *fio)
 	inc_page_count(fio->sbi, is_read_io(fio->op) ?
 			__read_io_type(page): WB_DATA_TYPE(fio->page));
 
+	f2fs_set_bio_ctx_fio(fio, bio);
 	__f2fs_submit_read_bio(fio->sbi, bio, fio->type);
 	return 0;
 }
+
+#ifdef CONFIG_F2FS_FS_ENCRYPTION
+static int f2fs_crypt_bio_not_mergeable(struct bio *bio, struct page *nxt)
+{
+	struct address_space *bio_mapping;
+	struct address_space *nxt_mapping;
+	struct page *p;
+
+	if (!bio || !nxt)
+		return 0;
+
+	p = bio_page(bio);
+
+	if (!p)
+		return 0;
+
+	bio_mapping = page_mapping(p);
+	nxt_mapping = page_mapping(nxt);
+
+	if (bio_mapping && nxt_mapping) {
+		if (!bio_mapping->host || !nxt_mapping->host)
+			return 0;
+
+		/* both not hw encrypted => don't care */
+		if (!fscrypt_is_hw_encrypt(bio_mapping->host) &&
+		    !fscrypt_is_hw_encrypt(nxt_mapping->host))
+			return 0;
+
+		/* different file => don't merge */
+		if (bio_mapping->host->i_ino != nxt_mapping->host->i_ino)
+			return 1;
+
+		/* discontiguous page index => don't merge */
+		if ((p->index + bio_segments(bio)) != (nxt->index))
+			return 1;
+	}
+
+	return 0;
+}
+#else
+static int f2fs_crypt_bio_not_mergeable(struct bio *bio struct page *nxt)
+{
+	return 0;
+}
+#endif
 
 void f2fs_submit_page_write(struct f2fs_io_info *fio)
 {
@@ -555,6 +606,9 @@ next:
 	if (io->bio && (io->last_block_in_bio != fio->new_blkaddr - 1 ||
 	    (io->fio.op != fio->op || io->fio.op_flags != fio->op_flags) ||
 			!__same_bdev(sbi, fio->new_blkaddr, io->bio)))
+		__submit_merged_bio(io);
+
+	if (f2fs_crypt_bio_not_mergeable(io->bio, bio_page))
 		__submit_merged_bio(io);
 
 alloc_new:
@@ -611,6 +665,11 @@ static struct bio *f2fs_grab_read_bio(struct inode *inode, block_t blkaddr,
 	bio->bi_end_io = f2fs_read_end_io;
 	bio_set_op_attrs(bio, REQ_OP_READ, op_flag);
 
+	if (fscrypt_is_hw_encrypt(inode)) {
+		f2fs_wait_on_block_writeback(inode, blkaddr);
+		return bio;
+	}
+
 	if (f2fs_encrypted_file(inode))
 		post_read_steps |= 1 << STEP_DECRYPT;
 	if (post_read_steps) {
@@ -647,6 +706,7 @@ static int f2fs_submit_page_read(struct inode *inode, struct page *page,
 	ClearPageError(page);
 	inc_page_count(F2FS_I_SB(inode), F2FS_RD_DATA);
 
+	f2fs_set_bio_ctx(inode, bio);
 	__f2fs_submit_read_bio(F2FS_I_SB(inode), bio, DATA);
 	return 0;
 }
@@ -1646,9 +1706,13 @@ zero_out:
 		if (bio && (last_block_in_bio != block_nr - 1 ||
 			!__same_bdev(F2FS_I_SB(inode), block_nr, bio))) {
 submit_and_realloc:
+			f2fs_set_bio_ctx(inode, bio);
 			__f2fs_submit_read_bio(F2FS_I_SB(inode), bio, DATA);
 			bio = NULL;
 		}
+
+		if (f2fs_crypt_bio_not_mergeable(bio, page))
+			goto submit_and_realloc;
 
 		if (bio == NULL) {
 			bio = f2fs_grab_read_bio(inode, block_nr, nr_pages,
@@ -1679,6 +1743,7 @@ set_error_page:
 		goto next_page;
 confused:
 		if (bio) {
+			f2fs_set_bio_ctx(inode, bio);
 			__f2fs_submit_read_bio(F2FS_I_SB(inode), bio, DATA);
 			bio = NULL;
 		}
@@ -1688,8 +1753,10 @@ next_page:
 			put_page(page);
 	}
 	BUG_ON(pages && !list_empty(pages));
-	if (bio)
+	if (bio) {
+		f2fs_set_bio_ctx(inode, bio);
 		__f2fs_submit_read_bio(F2FS_I_SB(inode), bio, DATA);
+	}
 	return 0;
 }
 
@@ -1735,6 +1802,9 @@ static int encrypt_one_page(struct f2fs_io_info *fio)
 
 	/* wait for GCed page writeback via META_MAPPING */
 	f2fs_wait_on_block_writeback(inode, fio->old_blkaddr);
+
+	if (fscrypt_is_hw_encrypt(inode))
+		return 0;
 
 retry_encrypt:
 	fio->encrypted_page = fscrypt_encrypt_page(inode, fio->page,
